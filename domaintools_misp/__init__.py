54bf3d1739b907015df2c50526dce9f39d05e745
JD Babac <jbabac@domaintools.com>
2023-04-06 03:50:43 +0800

    ID-1676: Add updates to 0.2.1 package.

--- domaintools_misp/__init__.py
+++ domaintools_misp/__init__.py
@@ -7,8 +7,6 @@ from domaintools_misp.iris_pivot import dt_misp_module_iris_pivot
 from domaintools_misp.pivot import dt_misp_module_pivot
 from domaintools_misp.historic import dt_misp_module_historic
 
-__version__ = "0.2.0"
-
 
 def register(mhandlers, loaded_modules):
     mhandlers.pop("domaintools", None)
--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
 """Python installation definition for misp"""
-import os
-import re
 from setuptools import setup
 
-PACKAGE = 'domaintools_misp'
-PACKAGE_PATH = os.path.join(os.path.dirname(__file__), PACKAGE)
-
-with open(os.path.join(PACKAGE_PATH, '__init__.py')) as package_file:
-    version_match = re.search(r'''^__version__\s*=\s*['"]([^'"]*)['"]''', package_file.read(), re.M)
-    if version_match:
-        VERSION = version_match.group(1)
-    else:
-        raise RuntimeError('No __version__ specified in {}'.format(package_file.name))
-
-setup(name=PACKAGE,
-      version=VERSION,
-      description='The DomainTools MISP library',
-      author='DomainTools',
-      author_email='support@domaintools.com',
-      url='http://www.domaintools.com/',
-      install_requires=['tldextract',
-        'python-dateutil',
-        'aiohttp',
-        'domaintools_api'],
-      packages=[PACKAGE])
+setup(
+    # All other parameters are in `pyproject.toml`
+    packages=["domaintools_misp"],
+)
